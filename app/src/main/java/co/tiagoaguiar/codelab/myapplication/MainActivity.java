b2e65d0ed2004e387f73f642f468d96503de4ba1
ERBCosta <eltonr@gmail.com>
2022-02-07 15:49:31 -0300

    Cores de fundo dos botões ainda não mudaram! Atualizado layout para tipo Grid. Agora é possível clicar em toda a área e não só no botão para a nova tela se abrir. Adicionado a tela TMB e os eventos de click.

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -20,74 +20,90 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-	//private View btnImc;
-	private RecyclerView rvMain;
+    private RecyclerView rvMain;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	//	btnImc = findViewById(R.id.btn_imc);
-		rvMain = findViewById(R.id.main_rv);
+        rvMain = findViewById(R.id.main_rv);
 
-		List<MainItem> mainItems = new ArrayList<>();
-		mainItems.add(new MainItem(1, R.drawable.ic_baseline_accessibility_24, R.string.label_imc, Color.BLUE));
-		mainItems.add(new MainItem(2, R.drawable.ic_baseline_visibility_24, R.string.label_tmb, Color.YELLOW));
+        List<MainItem> mainItems = new ArrayList<>();
+        mainItems.add(new MainItem(1, R.drawable.ic_baseline_accessibility_24, R.string.label_imc, Color.BLUE));
+        mainItems.add(new MainItem(2, R.drawable.ic_baseline_visibility_24, R.string.label_tmb, Color.YELLOW));
 
-		// Primeiro item: definir o comportamento de exibição do layout da recyclerview (mosaic, grid, linear horizontal ou vertical)
-		rvMain.setLayoutManager(new GridLayoutManager(this, 2));
-		MainAdapter adapter = new MainAdapter(mainItems);
-		rvMain.setAdapter(adapter);
+        // Primeiro item: definir o comportamento de exibição do layout da recyclerview (mosaic, grid, linear horizontal ou vertical)
+        rvMain.setLayoutManager(new GridLayoutManager(this, 2));
+        MainAdapter adapter = new MainAdapter(mainItems);
+        adapter.setListener(id -> {
+            switch (id) {
+                case 1:
+                    startActivity(new Intent(MainActivity.this, ImcActivity.class));
+                    break;
+                case 2:
+                    startActivity(new Intent(MainActivity.this, TmbActivity.class));
+                    break;
+            }
+        });
+        rvMain.setAdapter(adapter);
 
-	//	btnImc.setOnClickListener(view -> {
-	//		Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-	//		startActivity(intent);
+    }
 
-	//	});
-	}
+    private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
 
-	private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+        private List<MainItem> mainItems;
+        private OnItemClickListener listener;
 
-		private List<MainItem> mainItems;
+        public MainAdapter(List<MainItem> mainItems) {
+            this.mainItems = mainItems;
+        }
 
-		public MainAdapter(List<MainItem> mainItems) {
-			this.mainItems = mainItems;
-		}
+        public void setListener(OnItemClickListener listener) {
+            this.listener = listener;
+        }
 
-		@NonNull
-		@Override
-		public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-			return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
-		}
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+        }
 
-		@Override
-		public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
-			MainItem mainItemCurrent = mainItems.get(position);
-			holder.bind(mainItemCurrent);
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+            MainItem mainItemCurrent = mainItems.get(position);
+            holder.bind(mainItemCurrent);
 
-		}
+        }
 
-		@Override
-		public int getItemCount() {
-			return mainItems.size();
-		}
-	}
-//Entenda como sendo a VIEWS DA CÉLULA que está dentro da Recycler View
-	private class MainViewHolder extends RecyclerView.ViewHolder {
+        @Override
+        public int getItemCount() {
+            return mainItems.size();
+        }
 
-	public MainViewHolder(@NonNull View itemView) {
-		super(itemView);
-	}
-	public void bind(MainItem item) {
-		TextView txtName = itemView.findViewById(R.id.item_txt_name);
-		ImageView imgIcon = itemView.findViewById(R.id.item_img_icon);
-		LinearLayout container = (LinearLayout) itemView.findViewById(R.id.btn_imc);
+        //Entenda como sendo a VIEWS DA CÉLULA que está dentro da Recycler View
+        private class MainViewHolder extends RecyclerView.ViewHolder {
 
-		txtName.setText(item.getTextStringId());
-		imgIcon.setImageResource(item.getDrawableI());
-		container.setBackgroundColor(item.getColor());
+            public MainViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
 
-	}
-}
+            public void bind(MainItem item) {
+                TextView txtName = itemView.findViewById(R.id.item_txt_name);
+                ImageView imgIcon = itemView.findViewById(R.id.item_img_icon);
+                LinearLayout btnImc = (LinearLayout) itemView.findViewById(R.id.btn_imc);
+
+                btnImc.setOnClickListener(view -> {
+                    listener.onClick(item.getId());
+                });
+
+                txtName.setText(item.getTextStringId());
+                imgIcon.setImageResource(item.getDrawableI());
+                btnImc.setBackgroundColor(item.getColor());
+
+            }
+        }
+    }
+
+
 }
